62510adab5bf7792e9ccac2ffece784f05a1e281
theharuun <160717559+theharuun@users.noreply.github.com>
2024-06-10 16:39:19 +0300

    diger-odevler

---
+++ Online_Film_Sistemi/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Online_Film_Sistemi;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
+    }
+}
