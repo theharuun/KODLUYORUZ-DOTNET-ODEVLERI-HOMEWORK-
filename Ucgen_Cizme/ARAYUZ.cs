9ba7e7366285093224a79d783f0ec87c6b77083d
theharuun <160717559+theharuun@users.noreply.github.com>
2024-06-11 09:13:41 +0300

    Ucgen Cİzme odevi bitmistir.Kullanıcının girdigi verilere (taban ve yukseklik) gore ucgen dik ucgen cizer.

---
+++ Ucgen_Cizme/ARAYUZ.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucgen_Cizme
+{
+    internal class ARAYUZ
+    {
+        private int yukseklik;
+        public int Yukseklik {
+            get { return yukseklik; }
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("0 veya 0'dan kucuk deger giremezsiniz");
+                }
+                else
+                {
+                    yukseklik = value;
+                }
+            }
+        }
+        private int taban;
+        public int Taban
+        {
+            get { return taban; }
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("0 veya 0'dan kucuk deger giremezsiniz");
+                    
+                }
+                else
+                {
+                    taban = value;
+                }
+            }
+        }
+
+        public ARAYUZ() 
+        {
+          
+
+        }
+
+        public string Run() 
+        {
+
+
+
+            
+            Console.Write("Ücgenin taban uzunlugunu girin: ");
+            Taban = int.Parse(Console.ReadLine());
+
+            Console.Write("Ücgenin yüksekligini girin: ");
+            Yukseklik = int.Parse(Console.ReadLine());
+
+            // Üçgeni çizme
+            for (int i = 1; i <= yukseklik; i++)
+            {
+                // Yıldızları çizme
+                int starCount = (i * taban) / yukseklik;
+                for (int j = 0; j <= starCount; j++)
+                {
+                    Console.Write("*");
+                }
+                // Yeni satıra geçme
+                Console.WriteLine();
+            }
+
+            string a = "adana";
+
+            return a;
+        }
+        
+    }
+}
---
+++ Ucgen_Cizme/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucgen_Cizme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ARAYUZ AY=new ARAYUZ();
+            AY.Run();
+
+            Console.ReadLine();
+        }
+    }
+}
