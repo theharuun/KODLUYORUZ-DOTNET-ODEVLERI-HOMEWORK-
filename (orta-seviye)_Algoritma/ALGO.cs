64e68ec3fa88656d4ca3d0401a10ecfc5c7ce1a3
theharuun <160717559+theharuun@users.noreply.github.com>
2024-06-13 11:43:52 +0300

    (orta-seviye)_Algoritra sorusu cozuldu. Kullanicinin girdigi cumledeki kelimelerin ilk ve son harflerinin yerlerini degistirdik.

---
+++ (orta-seviye)_Algoritma/ALGO.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _orta_seviye__Algoritma
+{
+    internal class ALGO
+    {
+       
+        static string String_I_S_yerdegistirme(ArrayList list)
+        {
+            ArrayList hedefCumle = new ArrayList();
+            foreach (string item in list)
+            {
+                if (item.Length > 3)
+                {
+                    item.Replace(item[0], item[item.Length - 1]);
+                    char ilkHarf = item[0];
+                    char sonHarf = item[item.Length - 1];
+                    string ortadakiHarfler = item.Substring(1, item.Length - 2);
+                    string result = sonHarf + ortadakiHarfler + ilkHarf;
+                    hedefCumle.Add(result);
+                }
+                else
+                {
+                    hedefCumle.Add (item);
+                }
+            }
+            foreach (var item in hedefCumle)
+            {
+                Console.Write(item + " ");
+            }
+            return "s";
+        }
+        static string ArrayListToString(ArrayList list)
+        {
+            // ArrayList'i string dizisine dönüştürme
+            string[] array = (string[])list.ToArray(typeof(string));
+            // String ifadeleri boşluk ile birleştirme
+            return string.Join(" ", array);
+        }
+        public string[] Run(string cumle)
+        {
+           
+            ArrayList kelimeListesi = new ArrayList();
+ 
+            // Cümleyi boşluk karakterine göre ayır
+            string[] kelimeler = cumle.Split(' ');
+            // kelimeleri listeye ekledik
+
+            foreach (string kelime in kelimeler)
+            {
+                kelimeListesi.Add(kelime);
+            }
+
+
+            String_I_S_yerdegistirme(kelimeListesi);
+
+
+            return kelimeler;
+        }
+    }
+}
---
+++ (orta-seviye)_Algoritma/ARAYUZ.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _orta_seviye__Algoritma
+{
+    internal class ARAYUZ
+    {
+        private string girilen;
+        public string Girilen
+        {
+            get { return girilen; }
+
+            set
+            {
+                if (value != null)
+                {
+                    girilen = value;
+                }
+            }
+        }
+
+        public void RUN()
+        {
+            Console.WriteLine("Cumledeki Kelimenin ilk ve son harflerini degistirmek icin cumleyi yaziniz :");
+            string Girilen = Console.ReadLine();
+
+           ALGO czm = new ALGO();
+            czm.Run(Girilen);
+        }
+    }
+}
