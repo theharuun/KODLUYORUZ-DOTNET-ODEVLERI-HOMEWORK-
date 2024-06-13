0d2b79fcfd380689c3ee62c006187288a0218851
theharuun <160717559+theharuun@users.noreply.github.com>
2024-06-13 10:25:32 +0300

    (OrtaSeviye)İkili sayilari toplayi yazan eger ayni sayi ciktiysa o sayinin karesini yazan bu sayilari kullanicidan alan program yazildi.

---
+++ (orta-seviye)_Integer_Ikililerin_Toplami/ARAYUZ.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _orta_seviye__Integer_Ikililerin_Toplami
+{
+    internal class ARAYUZ
+    {
+        public void RUN()
+        {
+            Console.WriteLine("***********HOSGELDINIZ************\nkaç çift sayi giriceksiniz  \nOrnegin 6 cift giricegim yazarsaniz 12 sayi isteyecegiz sizden");
+            int SayiAdet = int.Parse(Console.ReadLine());
+            if (SayiAdet % 2 != 0)
+            {
+                Console.Write("Cift Sayi Girmelisiniz : ");
+                SayiAdet = int.Parse(Console.ReadLine());
+            }
+            // kaç adet çift sayı gireceğini aldık önce
+            Console.WriteLine(SayiAdet * 2 + " tane sayi yazicaksiniz :)) ");
+
+            ArrayList arrayToplam = new ArrayList(); // ikili çiftleri toplayıp koyacağımız arraylistimiz
+
+            int[] sayi = new int[SayiAdet * 2]; // alınan cift sayisina bagli cift sayi iceren array olusturduk
+
+            Toplam  tt =new Toplam();
+            tt.Run(arrayToplam, sayi);
+
+        }
+    }
+}
---
+++ (orta-seviye)_Integer_Ikililerin_Toplami/Toplam.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _orta_seviye__Integer_Ikililerin_Toplami
+{
+    internal class Toplam
+    {
+        public void Run(ArrayList arrayToplam, int[] sayi)
+        {
+            for (int i = 0; i < sayi.Length; i++) // her sayiyi sirayla istedik
+            {
+                Console.Write(i + 1 + ".SAYI: ");
+                sayi[i] = int.Parse(Console.ReadLine()); Console.WriteLine();
+            }
+
+            for (int i = 0; i < sayi.Length; i = i + 2)//her ikili cifti topladik ve eger aynı olan varsa onun karesini aldik ve arraylistine ekledik
+            {
+                int toplam = sayi[i] + sayi[i + 1];
+                if (arrayToplam.Contains(toplam))
+                {
+                    toplam *= toplam;
+                    arrayToplam.Add(toplam);
+                }
+                else
+                {
+                    arrayToplam.Add(toplam);
+                }
+
+            }
+            foreach (int i in arrayToplam) { Console.Write(i + " - "); }
+        }
+
+    }
+}
