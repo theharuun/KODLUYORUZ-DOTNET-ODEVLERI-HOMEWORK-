b5ca48a43c6fd7a3e2390a82624e6796ba789a5c
theharuun <160717559+theharuun@users.noreply.github.com>
2024-06-13 10:53:37 +0300

    (orta-seviye) mutlak alma UYGULMASI bitti. kullanicinin girdigi sayilarin 67den kucukse farklarinin topladik , buyukse karelerini alip topladik.

---
+++ (Orta-Seviye)_Mutlka_Kare_Alma/ARAYUZ.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Orta_Seviye__Mutlka_Kare_Alma
+{
+    internal class ARAYUZ
+    {
+        public void RUN()
+        {
+            Console.Write("KAÇ ADET SAYI GİRMEK İSTERSİNİZ : ");
+            int SayiAdet=int.Parse(Console.ReadLine());
+
+            int[] Sayilar=new int[SayiAdet];
+
+              for (int i = 0; i < Sayilar.Length; i++) // her sayiyi sirayla istedik
+                {
+                    Console.Write(i + 1 + ".SAYI: ");
+                    Sayilar[i] = int.Parse(Console.ReadLine()); Console.WriteLine();
+                    
+            }
+
+            MUTLAK MT=new MUTLAK();
+            MT.run(Sayilar);
+            
+        }
+    }
+}
---
+++ (Orta-Seviye)_Mutlka_Kare_Alma/MUTLAK.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Orta_Seviye__Mutlka_Kare_Alma
+{
+    internal class MUTLAK
+    {
+        int toplam = 0;
+        int mutlak = 0;
+        public void run(int[] Sayilar)
+        {
+            for(int i = 0; i < Sayilar.Length; i++)
+            {
+                if(Sayilar[i] < 67)
+                {
+                    toplam+=67-Sayilar[i];
+                }
+                else if(Sayilar[i] >67)
+                {
+                    int kare= (Sayilar[i] - 67) * (Sayilar[i]-67);
+                    mutlak += kare;
+                }
+            }
+            Console.WriteLine("TOPLAM : "+toplam+" -  MUTLAK: "+mutlak);
+        } 
+
+        
+    }
+}
