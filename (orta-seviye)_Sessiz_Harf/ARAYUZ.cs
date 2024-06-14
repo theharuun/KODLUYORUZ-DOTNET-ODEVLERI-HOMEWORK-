b4c91dd1a9e70798571a4a62627cb17ea435e678
theharuun <160717559+theharuun@users.noreply.github.com>
2024-06-14 11:20:01 +0300

    (orta-seviye)_SESSIZ HARF uygulamasi bitti kullanicinin girdigi cumledeki kelimelerde ikii unsuz art arda geliyorsa TRUE gelmiyorsa FALSE yazdiran uygulama bitti.

---
+++ (orta-seviye)_Sessiz_Harf/ARAYUZ.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _orta_seviye__Sessiz_Harf
+{
+    internal class ARAYUZ
+    {
+        private string girilen;
+        public string Girilen
+        {
+            get { return girilen; }
+
+            set
+            {
+                if (value != null)
+                {
+                    girilen = value;
+                }
+            }
+        }
+
+        public void RUN()
+        {
+            Console.WriteLine("Cumledeki Kelimelerde hangisinde bir unlu bir unsuz harf vardır bulmak icin cumle yaziniz :");
+            string Girilen = Console.ReadLine();
+
+            SESSIZ czm = new SESSIZ();
+            czm.Run(Girilen);
+        }
+    }
+}
---
+++ (orta-seviye)_Sessiz_Harf/SESSIZ.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _orta_seviye__Sessiz_Harf
+{
+    internal class SESSIZ
+    {
+
+        public void ciftSessiz(ArrayList kelimelistesi) 
+        {
+            char[] unluHarf = { 'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü' };
+
+            foreach (string kelime in kelimelistesi)
+            {
+                bool donus = false;
+
+                for (int j = 0; j < kelime.Length - 1; j++)
+                {
+                    char harf = kelime[j];
+                    char sonrakiHarf = kelime[j + 1];
+
+                    if (!IsUnluHarf(harf, unluHarf) && !IsUnluHarf(sonrakiHarf, unluHarf))
+                    {
+                        donus = true;
+                        break;
+                    }
+                }
+
+                Console.Write( (donus ? " True " : " False "));
+            }
+        }
+
+        private static bool IsUnluHarf(char harf, char[] unluHarf)
+        {
+            foreach (char uh in unluHarf)
+            {
+                if (harf == uh)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void Run(string cumle)
+        {
+            ArrayList kelimeListesi = new ArrayList();
+
+            string[] kelime=cumle.Split(' ');
+
+            foreach (string s in kelime) 
+            {
+                kelimeListesi.Add(s);
+            }
+            
+            ciftSessiz(kelimeListesi);
+        }
+    }
+}
